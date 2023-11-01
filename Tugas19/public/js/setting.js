a1bf34185b85a33287a90536da48b551ff7419ed
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2023-11-01 11:30:05 +0700

    Tugas 19

--- Tugas19/public/js/setting.js
+++ Tugas19/public/js/setting.js
@@ -1,5 +1,5 @@
  function confirmation(name) {
-    const answer = confirm(`Apakah kamu yakin akan menghapus data '${name} ?`)
+    const answer = confirm(`Apakah kamu yakin akan menghapus data '${name}'?`)
     if(answer) return true
     else return false
  }
